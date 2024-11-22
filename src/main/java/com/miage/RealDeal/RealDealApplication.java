23f12a1e78cf1807b0cb4de9d6822f576011f0b3
Maxime Brasley <75072652+maxime-brsl@users.noreply.github.com>
2024-11-22 09:48:44 +0100

    premiere mise en place

---
+++ src/main/java/com/miage/RealDeal/RealDealApplication.java
@@ -0,0 +1,13 @@
+package com.miage.RealDeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealDealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealDealApplication.class, args);
+	}
+
+}
