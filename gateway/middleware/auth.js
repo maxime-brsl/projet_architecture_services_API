dcb92310b4b9d9226457a58f0dbae8a319ddf040
jbrht <julie.barthet2@gmail.com>
2025-01-06 16:23:30 +0100

    modif css et utilisation du auth

--- gateway/middleware/auth.js
+++ gateway/middleware/auth.js
@@ -7,12 +7,12 @@ import dotenv from 'dotenv';
 const SECRET_KEY = "api_projet_pas_super_secure_le_secret_key_mais_bon";
 
 export const verifyToken = (req, res, next) => {
-
-    const token = req.headers['authorization'];
+    const token = req.headers['authorization'].split(' ')[1]; //permet de retirer le Bearer
     if (!token) return res.status(401).json({ message: 'Non autorisé' });
 
     try {
         req.user = jwt.verify(token, SECRET_KEY);
+        console.log("req.user", req.user)
         req.headers['Authorization'] = token;
         next();
     } catch (err) {
